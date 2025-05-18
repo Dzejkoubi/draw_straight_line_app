74c77e412c1b29a4b990fc935e3d44ee9ca4fe2d
Dzejkoubi <kubakraus06@gmail.com>
2025-05-18 09:44:49 +0200

    feat: initialized supabase

--- lib/screens/profile_screen/profile_screen.dart
+++ lib/screens/profile_screen/profile_screen.dart
@@ -2,10 +2,13 @@ import 'package:auto_route/annotations.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:draw_straight_line_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 @RoutePage()
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  ProfileScreen({super.key});
+
+  final _future = Supabase.instance.client.from('instruments').select();
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +22,21 @@ class ProfileScreen extends StatelessWidget {
           },
         ),
       ),
-      body: Center(child: Text("User profile")),
+      body: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+          final instruments = snapshot.data!;
+          return ListView.builder(
+            itemCount: instruments.length,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text(instruments[index]['name']));
+            },
+          );
+        },
+      ),
     );
   }
 }
